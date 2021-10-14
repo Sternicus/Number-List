86d31dd6c6cb6e1e4bca0453139f177599126df0
Sternicus <90805907+Sternicus@users.noreply.github.com>
2021-10-14 16:00:05 -0400

    Create NumberList.py

---
+++ NumberList.py
@@ -0,0 +1,11 @@
+x = int(input("Please type a number"))
+
+y= int(input("Please type another number"))
+
+z= int(input("Please type another number"))
+
+list = [x,y,z]
+
+list.sort(reverse=False)
+
+print("Here are your numbers in order!:",list)
